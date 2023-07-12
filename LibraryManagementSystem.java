6237e12b4232c57d1a750873be16d7975656a69e
Senal Dilanjana KAD <138982586+5yndr0m@users.noreply.github.com>
2023-07-12 14:33:49 +0000

    adding objects instead of arrays

--- LibraryManagementSystem.java
+++ LibraryManagementSystem.java
@@ -1,30 +1,32 @@
 public class LibraryManagementSystem {
     public static void main(String[] args) {
-        // Create 5 objects of the Book class
-        Book[] books = {
-                new Book("Python Crash Course", "B001", "Eric Matthes", 544),
-                new Book("The Great Gatsby", "B002", "F. Scott Fitzgerald", 180),
-                new Book("To Kill a Mockingbird", "B003", "Harper Lee", 324),
-                new Book("1984", "B004", "George Orwell", 328),
-                new Book("The Catcher in the Rye", "B005", "J.D. Salinger", 234)
-        };
+        
+        //1.Create 5 objects of the Book class, each representing a different book in the library.
+       
+        Book book1 = new Book("The Catcher in the Rye", "B001", "J.D. Salinger", 240);
+        Book book2 = new Book("To Kill a Mockingbird", "B002", "Harper Lee", 281);
+        Book book3 = new Book("1984", "B003", "George Orwell", 328);
+        Book book4 = new Book("Pride and Prejudice", "B004", "Jane Austen", 432);
+        Book book5 = new Book("The Hobbit", "B005", "J.R.R.Tolkein", 320);
 
-        // Create 5 objects of the Magazine class
+        Book[] books ={book1,book2,book3,book4,book5};
+
+        //2. Create 5 objects of the Magazine class, each representing a different magazine in the library.
         Magazine[] magazines = {
-                new Magazine("National Geographic", "M001", "July 2023", "National Geographic Society"),
-                new Magazine("Time", "M002", "June 2023", "Time USA, LLC"),
-                new Magazine("Vogue", "M003", "August 2023", "Condé Nast"),
-                new Magazine("The Economist", "M004", "July 2023", "The Economist Group"),
-                new Magazine("Sports Illustrated", "M005", "July 2023", "ABG-SI LLC")
+                new Magazine("National Geographic", "M001", "August 2023", "National Geographic Society"),
+                new Magazine("Time", "M002", "September 2023", "Time USA, LLC"),
+                new Magazine("Forbes", "M003", "June 2023", "Forbes Media"),
+                new Magazine("Vogue", "M004", "July 2023", "Condé Nast"),
+                new Magazine("Sports Illustrated", "M005", "July 2023", "Maven Coalition")
         };
 
-        // Create 5 objects of the LibraryMember class
+        //3. Create 5 objects of the LibraryMember class, each representing a different member of the library.
         LibraryMember[] members = {
                 new LibraryMember("L001", "John Doe"),
                 new LibraryMember("L002", "Jane Smith"),
-                new LibraryMember("L003", "Michael Johnson"),
-                new LibraryMember("L004", "Emily Davis"),
-                new LibraryMember("L005", "Robert Brown")
+                new LibraryMember("L003", "David Johnson"),
+                new LibraryMember("L004", "Sarah Williams"),
+                new LibraryMember("L005", "Michael Brown")
         };
 
         // Check out and check in an item
